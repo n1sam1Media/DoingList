49340a7b4c1b491165d1cf18f04db48302ffb64c
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-09-19 07:11:28 +0200

    Changed li s decoration code to a effective less code method using jQuery

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,15 +1,4 @@
 //check off specific todos by clicking
 $("li").click(function(){
-  //if li is gray
-  if($(this).css("color") === "rgb(128, 128, 128)") {
-    $(this).css({
-      color: "black",
-      textDecoration: "none"
-    });
-  } else {
-    $(this).css({
-      color: "gray",
-      textDecoration: "line-through"
-    });
-  }
+  $(this).toggleClass("completed");
 });
