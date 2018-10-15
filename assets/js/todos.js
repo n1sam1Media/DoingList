180fb9e6904fbf7b8bfea962b219ed5b67ecfa46
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-10-15 19:39:36 +0200

    added delete feature

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,8 +1,25 @@
 //check off specific todos by clicking
 $("li").click(function(){
   //if li is gray
-  console.log($(this).css("color"));
   if($(this).css("color") === "rgb(128, 128, 128)"){
-      console.log("It is corrently gray!");
+      //turn it black
+      $(this).css({
+        color: "black",
+        textDecoration: "none"
+      });
+  } else {
+    //turn it gray
+    $(this).css({
+      color: "gray",
+      textDecoration: "line-through"
+    });
   }
+});
+
+//click X to delete a todo
+$("span").click(function(event) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  event.stopPropagation();
 });
