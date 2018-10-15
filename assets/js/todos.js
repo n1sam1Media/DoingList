e4a1860bf6de83f62bd276c4e4adef4906fac8c4
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-10-15 20:17:09 +0200

    added the features to add new todos and delete newly added todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,5 +1,5 @@
 //check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   //if li is gray
   if($(this).css("color") === "rgb(128, 128, 128)"){
       //turn it black
@@ -17,9 +17,23 @@ $("li").click(function(){
 });
 
 //click X to delete a todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if(event.which === 13) {
+    //console.log("You hit enter!");
+    //extracting value
+    $(this).val();
+    //save extracted value to a variable 
+    var todoText = $(this).val();
+    //clearing text from input
+    $(this).val("");
+    //create a new li and add to ul
+    $("ul").append("<li>" + "<span>X </span>" + todoText + "</li");
+  }
 });
