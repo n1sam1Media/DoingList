102578349efd151a699c290a54044eed854ab172
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-10-15 22:06:36 +0200

    new todo feature added

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -34,6 +34,6 @@ $("input[type='text']").keypress(function(event) {
     //clearing text from input
     $(this).val("");
     //create a new li and add to ul
-    $("ul").append("<li>" + "<span>X </span>" + todoText + "</li");
+    $("ul").append("<li>" + "<span><i class='fa fa-trash'></i> </span>" + todoText + "</li");
   }
 });
